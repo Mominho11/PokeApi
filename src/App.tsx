60f83353ec3a499f8952da607d66a71c1d7973ed
MohamedB <mohamed.berrabah11@gmail.com>
2024-12-17 16:56:54 +0100

    Save commit fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
 import './App.css'
 import Footer from "./components/Footer.tsx";
+import {useEffect, useState} from "react";
 
 function App() {
+  const [pokemon, setPokemon] = useState([]);
+
+  useEffect(() => {
+      const fetchDatas = async () => {
+          const response = await fetch("https://pokeapi.co/api/v2/pokemon?offset=40&limit=10");
+          const data = await response.json();
+          setPokemon(data);
+      }
+      fetchDatas();
+
+  }, [])
+
+    useEffect(() => {
+        console.log("AHHHHHHHHHHHH ", pokemon);
+    }, [pokemon]);
+
   return (
       <main>
           <h1>Welcome to our beautiful Pokedex</h1>
+
+
           <section className="search-bar-input">
               <input type="text" placeholder="Looking for a pokemon ?"/>
               <button><img src="../public/pokeball-pokemon-svgrepo-com.svg" alt="Pokeball-Button"/></button>
