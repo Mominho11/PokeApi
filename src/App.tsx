9bd46bce6e21eb037cc5ab4342c5031d72a06496
MohamedB <mohamed.berrabah11@gmail.com>
2024-12-17 15:51:57 +0100

    Updated responsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -8,30 +8,72 @@ function App() {
               <input type="text" placeholder="Looking for a pokemon ?"/>
               <button><img src="../public/pokeball-pokemon-svgrepo-com.svg" alt="Pokeball-Button"/></button>
           </section>
-          <section className="poke-card">
-              <h2>Dracaufeu</h2>
-              <img src="../public/Dracaufeu.png" alt="Dracaufeu"/>
-              <p>Meilleur pokemon de l'histoire des pokemon</p>
-              <button>More ...</button>
-          </section>
-          <section className="poke-card">
-              <h2>Dracaufeu</h2>
-              <img src="../public/Dracaufeu.png" alt="Dracaufeu"/>
-              <p>Meilleur pokemon de l'histoire des pokemon</p>
-              <button>More ...</button>
-          </section>
-          <section className="poke-card">
-              <h2>Dracaufeu</h2>
-              <img src="../public/Dracaufeu.png" alt="Dracaufeu"/>
-              <p>Meilleur pokemon de l'histoire des pokemon</p>
-              <button>More ...</button>
-          </section>
-          <section className="poke-card">
-              <h2>Dracaufeu</h2>
-              <img src="../public/Dracaufeu.png" alt="Dracaufeu"/>
-              <p>Meilleur pokemon de l'histoire des pokemon</p>
-              <button>More ...</button>
-          </section>
+
+          <ul className="poke-card-container">
+              <li className="poke-card">
+                  <h2>Dracaufeu</h2>
+                  <img src="../public/Dracaufeu.png" alt="Dracaufeu"/>
+                  <p>Meilleur pokemon de l'histoire des pokemon</p>
+                  <button>More ...</button>
+              </li>
+
+              <li className="poke-card">
+                  <h2>Mega Dracaufeu X</h2>
+                  <img src="../public/Dracaufeu.png" alt="Dracaufeu"/>
+                  <p>Meilleur pokemon de l'histoire des pokemon</p>
+                  <button>More ...</button>
+              </li>
+
+              <li className="poke-card">
+                  <h2>Dracaufeu</h2>
+                  <img src="../public/Dracaufeu.png" alt="Dracaufeu"/>
+                  <p>Meilleur pokemon de l'histoire des pokemon</p>
+                  <button>More ...</button>
+              </li>
+
+              <li className="poke-card">
+                  <h2>Dracaufeu</h2>
+                  <img src="../public/Dracaufeu.png" alt="Dracaufeu"/>
+                  <p>Meilleur pokemon de l'histoire des pokemon</p>
+                  <button>More ...</button>
+              </li>
+
+              <li className="poke-card">
+                  <h2>Dracaufeu</h2>
+                  <img src="../public/Dracaufeu.png" alt="Dracaufeu"/>
+                  <p>Meilleur pokemon de l'histoire des pokemon</p>
+                  <button>More ...</button>
+              </li>
+
+              <li className="poke-card">
+                  <h2>Dracaufeu</h2>
+                  <img src="../public/Dracaufeu.png" alt="Dracaufeu"/>
+                  <p>Meilleur pokemon de l'histoire des pokemon</p>
+                  <button>More ...</button>
+              </li>
+
+              <li className="poke-card">
+                  <h2>Dracaufeu</h2>
+                  <img src="../public/Dracaufeu.png" alt="Dracaufeu"/>
+                  <p>Meilleur pokemon de l'histoire des pokemon</p>
+                  <button>More ...</button>
+              </li>
+
+              <li className="poke-card">
+                  <h2>Dracaufeu</h2>
+                  <img src="../public/Dracaufeu.png" alt="Dracaufeu"/>
+                  <p>Meilleur pokemon de l'histoire des pokemon</p>
+                  <button>More ...</button>
+              </li>
+
+              <li className="poke-card">
+                  <h2>Dracaufeu</h2>
+                  <img src="../public/Dracaufeu.png" alt="Dracaufeu"/>
+                  <p>Meilleur pokemon de l'histoire des pokemon</p>
+                  <button>More ...</button>
+              </li>
+          </ul>
+
       </main>
   )
 }
