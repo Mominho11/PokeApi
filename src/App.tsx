50e9fa5d0ee8a1b8099b9cd22ba88f5c03158d42
Neogiciaa <chevalier.stepheh@hotmail.com>
2024-12-18 16:40:21 +0100

    Save commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,19 @@
 import './App.css'
 import {useEffect, useRef, useState} from "react";
 import Pokemon from "./interfaces/Pokemon.tsx";
-import pokemon from "./interfaces/Pokemon.tsx";
 
 function App() {
     const [pokemons, setPokemons] = useState([]);
-    // const [pokemonPhoto, setPokemonPhoto] = useState([]);
     const [visible, setVisible] = useState(false);
     const modalRef = useRef<HTMLDivElement | null>(null);
     const [selectedPokemon, setSelectedPokemon] = useState<Pokemon | null>(null);
+    const [selectedDescription, setSelectedDescription] = useState<string | null>(null);
 
     useEffect(() => {
         const fetchDatas = async () => {
-            const response = await fetch("https://pokeapi.co/api/v2/pokemon?offset=0&limit=10?lang=fr");
+            const response = await fetch("https://pokeapi.co/api/v2/pokemon?offset=0&limit=12?lang=fr");
             const data = await response.json();
-            const filteredDatas = data.results.filter((pokemon) => pokemon.name);
+            const filteredDatas = data.results.filter((pokemon: Pokemon) => pokemon.name);
 
             for (const pokemon of filteredDatas) {
                 const response = await fetch(pokemon.url);
@@ -26,8 +25,6 @@ function App() {
                     }
                     return previousPokemon;
                 });
-
-                // Fetch photos sur base de la logique au dessus !
             }
         }
         fetchDatas();
@@ -58,52 +55,67 @@ function App() {
     };
 
     useEffect(() => {
-        console.log("Selected poké", selectedPokemon);
+        const fetchDatas = async () => {
+            const response = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${selectedPokemon.name}`);
+            const data = await response.json();
+            setSelectedDescription(data.flavor_text_entries[0].flavor_text);
+            console.log("Desc -> ", selectedDescription);
+        }
+        fetchDatas();
 
     }, [selectedPokemon]);
 
-return (
-    <main>
-        <h1>Welcome to our beautiful Pokedex</h1>
+    return (
+        <main>
+            <h1>Welcome to our beautiful Pokedex</h1>
 
-        <section className="search-bar-input">
-            <input type="text" placeholder="Looking for a pokemon ?"/>
-            <button><img src="../public/pokeball-pokemon-svgrepo-com.svg" alt="Pokeball-Button"/></button>
-        </section>
+            <section className="search-bar-input">
+                <input type="text" placeholder="Looking for a pokemon ?"/>
+                <button><img src="../public/pokeball-pokemon-svgrepo-com.svg" alt="Pokeball-Button"/></button>
+            </section>
 
-        <ul className="poke-card-container">
-            {pokemons.map((pokemon: Pokemon) => (
-                <li className="poke-card" key={pokemon.id}>
-                    <h2>{pokemon.name}</h2>
-                    <img src="" alt="Tempo"></img>
-                    <p>{pokemon.types.map((type: Pokemon.types) => type.type.name).join(' - ')}</p>
-                    <button className="open-details-button" onClick={() => {
-                        setSelectedPokemon(() => pokemons.find((p) => p.name === pokemon.name));
-                        setVisible(!visible);
-                    }} type="button">More . . .
-                    </button>
-                </li>
-            ))}
-        </ul>
+            <ul className="poke-card-container">
+                {pokemons.map((pokemon: Pokemon) => (
+                    <li className="poke-card" key={pokemon.id}>
+                        <h2>{pokemon.name}</h2>
+                        <img src={pokemon.sprites.front_default} alt={pokemon.name}></img>
+                        <p className="test-type">{pokemon.types.map((type: Pokemon.types) => type.type.name).join(' - ')}</p>
+                        <button className="open-details-button" onClick={() => {
+                            setSelectedPokemon(() => pokemons.find((p) => p.name === pokemon.name));
+                            setVisible(!visible);
+                        }} type="button">More . . .
+                        </button>
+                    </li>
+                ))}
+            </ul>
 
-        {visible && selectedPokemon && (
-            <section className="poke-details-modal"   ref={modalRef} onClick={handleModalClick}>
+            {visible && selectedPokemon && (
+                <section className="poke-details-modal" ref={modalRef} onClick={handleModalClick}>
                     <article>
                         <h2>You selected {selectedPokemon.name} !</h2>
-                        <img src={selectedPokemon.sprites.front_shiny} alt={`Image for ${selectedPokemon.name}`}></img>
+
+                        <div className="picture-container">
+                            <img src={selectedPokemon.sprites.front_default}
+                                 alt={`Image for ${selectedPokemon.name} normal`}></img>
+                        </div>
+
+                        <section className="pokemon-infos">
+                            <p>N° {selectedPokemon.id} Pokémon FakeFlamme Height: {selectedPokemon.height} Weight: {selectedPokemon.weight}</p>
+                        </section>
+
+                        <p>{selectedDescription!}</p>
                         {/*<p>{pokemon.types.map((type) => type.type.name.join(' - '))}</p>*/}
                     </article>
 
 
+                    {/*<h2>You selected {pokemons[0].name} !</h2>*/}
+                    {/*<img src="" alt="Tempo"></img>*/}
+                    {/*<p>{pokemons.types.map((type) => type.type.name).join(' - ')}</p>*/}
 
-                {/*<h2>You selected {pokemons[0].name} !</h2>*/}
-                {/*<img src="" alt="Tempo"></img>*/}
-                {/*<p>{pokemons.types.map((type) => type.type.name).join(' - ')}</p>*/}
-
-            </section>
-        )}
-    </main>
-)
+                </section>
+            )}
+        </main>
+    )
 }
 
 export default App
